71e4ebb2483a16997df857e5f0f25e2893102d60
vivien <vivienlord1@gmail.com>
2021-04-29 05:31:12 +0100

    Weather App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import LogIn from './Comoponents/LogIn';
+import Weather from './Comoponents/Weather';
+
+function App() {
+  return (
+    <div className="App">
+       <h3>WATHER APP</h3>
+       <form getweather={fetchData}/>
+       {console.groupCollapsed(weather.com)}
+    </div>
+  );
+}
+
+export default App;
